446f663c54d6f338e3168c4d6aef86afd1b52e80
ratataca <55879896+ratataca@users.noreply.github.com>
2021-07-23 23:13:51 +0900

    Update Lab1_acc_move.py

--- 실습/Lab1_acc_move.py
+++ 실습/Lab1_acc_move.py
@@ -1,10 +1,3 @@
-## * 주제 *
-## 햄스터 로봇의 속도를 줄여보자
-
-
-## 과제 2 :  햄스터의 속도를 빠르게 감소하는 방법을 생각해보자
-
-
 from roboid import *
 
 hamster = Hamster()
@@ -13,7 +6,6 @@ hamster = Hamster()
 for i in range(100, 0, -1):
     hamster.wheels(i, i)
     wait(30)
-
 
 
 hamster.stop()
